3c253f15d25fb9e8c5403db4a07ebd0756776383
Raja Ravindra Kathi <kr-ravindra@Mac-1178.lan>
2025-05-04 17:28:48 -0700

    Image fix on user list

--- app/components/UserToken.js
+++ app/components/UserToken.js
@@ -5,7 +5,7 @@ function UserToken({ token, amount, name, image }) {
     <button onClick={() => toggleTrade(token)} className="token">
     <div className="token__details" >
      
-      <img src={`https.me/files/${image}.png?token=12345`} alt={name} width={128} height={128} />
+      <img src={`https://kr-r.me/files/${image}.png?token=12345`} alt={name} width={128} height={128} />
       <p>Token: {token.slice(0, 6) + "..." + token.slice(38, 42)}</p>
       <p>Amount: {ethers.formatUnits(amount,18)}</p>
       <p className="name">{name}</p>
